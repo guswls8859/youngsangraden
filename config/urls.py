f2b1392d19e6d38095c759249e661830d8b52f01
guswls8859 <guswls8859@naver.com>
2026-04-16 11:40:14 +0900

    Add health check endpoint for Cloudtype

--- config/urls.py
+++ config/urls.py
@@ -5,8 +5,15 @@ from django.conf.urls.static import static
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 from django.views.generic import RedirectView
+from django.http import HttpResponse
+
+
+def health_check(request):
+    return HttpResponse('ok')
+
 
 urlpatterns = [
+    path('health/', health_check),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('reports/', include('reports.urls')),
